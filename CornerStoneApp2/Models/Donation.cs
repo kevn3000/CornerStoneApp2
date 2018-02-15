59fbd12af823d14a9a54181448351ccf6dc95a45
Lance Akins <dlakinsii@gmail.com>
2018-02-15 16:44:10 -0500

    Enabled Code First Migrations

---
+++ CornerStoneApp2/Models/Donation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CornerStoneApp2.Models
+{
+    public class Donation
+    {
+        [Key]
+        public int DonationID;
+        public float Amount;
+        //public int DonorID;
+    }
+}
