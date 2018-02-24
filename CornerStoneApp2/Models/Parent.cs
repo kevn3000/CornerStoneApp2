d789fed550e42d050d934eb9f4a54b9e93e44520
Kevin <n01374667@ospreys.unf.edu>
2018-02-23 20:56:42 -0500

    Parent
    Added more fields to parents

--- CornerStoneApp2/Models/Parent.cs
+++ CornerStoneApp2/Models/Parent.cs
@@ -11,8 +11,12 @@ namespace CornerStoneApp2.Models
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public string MidName { get; set; }
-
         public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
 
         public virtual ICollection<Child>Children { get; set; }
 
